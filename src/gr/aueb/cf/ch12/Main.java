4ed5429e42bec6b000ccf4b026a86ebe8a711503
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-01-29 12:22:41 +0200

    Changes

--- src/gr/aueb/cf/ch12/Main.java
+++ src/gr/aueb/cf/ch12/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Teacher teacher = new Teacher(1, "Athanasios", "Androutsos");
         User user = new User(1, "Costas", "Mask", "cmask", "12345", true);
+        User user2 = new User(2, "Alice", "W", "alicew", "1234567", true);
         Customer customer = new Customer(1, "Andrew", "Dimitriou", "076543289", "6906268255", "West Attica", "Attica","Athens", "Patission", "76", "10434");
         Point point = new Point(0, 10);
         Order order = new Order(1, 5.5, "Meat", "Ordered");

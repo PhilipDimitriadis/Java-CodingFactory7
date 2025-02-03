b29173d9b5566c577db1360bbc0eeb0ff1bd73f9
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-02-03 21:11:40 +0200

    Updates

--- src/gr/aueb/cf/ch15/Main.java
+++ src/gr/aueb/cf/ch15/Main.java
@@ -6,12 +6,17 @@ public class Main {
 
         ISpeakable cat = new Cat("Alice");
         ISpeakable dog = new Dog("Bob");
+        GenericSpeakingSchool genericSpeakingSchool1 = new GenericSpeakingSchool(cat);
+        GenericSpeakingSchool genericSpeakingSchool2 = new GenericSpeakingSchool(dog);
 
-        cat.speak();
-        dog.speak();
+//        cat.speak();
+//        dog.speak();
+//
+//        doSpeak(cat);
+//        doSpeak(dog);
 
-        doSpeak(cat);
-        doSpeak(dog);
+        genericSpeakingSchool1.learnToSpeak();
+        genericSpeakingSchool2.learnToSpeak();
     }
 
     public static void doSpeak(ISpeakable iSpeakable) {

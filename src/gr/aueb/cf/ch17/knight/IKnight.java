aca34fa166fccc01db849d3a87b1ab49194b2ee0
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-02-12 12:52:25 +0200

    Updates

---
+++ src/gr/aueb/cf/ch17/knight/IKnight.java
@@ -0,0 +1,6 @@
+package gr.aueb.cf.ch17.knight;
+
+public interface IKnight {
+
+    void embarkOnMission(IMission mission);
+}
---
+++ src/gr/aueb/cf/ch17/knight/IMission.java
@@ -0,0 +1,6 @@
+package gr.aueb.cf.ch17.knight;
+
+public interface IMission {
+
+    void embark();
+}

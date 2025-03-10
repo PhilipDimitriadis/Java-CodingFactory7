f580e0a2d92b9cbc1e89c6273461019124ea1dd3
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-03-10 16:16:15 +0200

    Updates

---
+++ src/gr/aueb/cf/ch19/singly_linked_list/SingleList.java
@@ -0,0 +1,61 @@
+package gr.aueb.cf.ch19.singly_linked_list;
+
+public class SingleList<T> {
+    private Node<T> head = null;
+
+
+    /**
+     * time-complexity O(1)
+     * @param t the value to be inserted
+     */
+    public void insertFirst(T t) {
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(head);
+
+        head = tmp;
+    }
+
+    public void insertLast(T t) {
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(null);
+
+        if (head == null) {
+            insertFirst(t);
+            return;
+        }
+
+        Node<T> n;
+        for (n = head; n.getNext() != null; n = n.getNext()) {
+        }
+        n.setNext(tmp);
+    }
+
+
+    /**
+     * Time complexity O(1)
+     * @return
+     */
+    public Node<T> removeFirst() {
+        if (head == null) return null;
+
+        Node<T> node = head;
+        head = head.getNext();
+        return node;
+    }
+
+    public Node<T> removeLast() {
+        if (head == null || head.getNext() == null) {
+            return removeFirst();
+        }
+
+        Node<T> n;
+        for (n = head; n.getNext().getItem() != null; n = n.getNext()) {
+        }
+
+        Node<T> nodeToReturn = n.getNext();
+        n.setNext(null);
+        return nodeToReturn;
+    }
+}

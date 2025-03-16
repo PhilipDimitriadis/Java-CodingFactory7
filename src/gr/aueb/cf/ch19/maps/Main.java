03c0aa47f4fc36a4659d1294d4660ba7a8fa2e18
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-03-16 16:30:22 +0200

    Updates

--- src/gr/aueb/cf/ch19/maps/Main.java
+++ src/gr/aueb/cf/ch19/maps/Main.java
@@ -10,13 +10,19 @@ public class Main {
 
         Map<String, String> countries = new HashMap<>();
 
+        // Add
         countries.put("GR", "Greece");
         countries.put("EN", "England");
         countries.put("US", "United States");
 
+        // Update
+        countries.put("US", "U. States");
+
+        // Get
         String value = countries.get("GR");
         System.out.println(value);
 
+        // Iterate
         Iterator<Map.Entry<String, String>> it = countries.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry<String, String> entry = it.next();
@@ -28,5 +34,7 @@ public class Main {
         }
 
         countries.forEach((k, v) -> System.out.println("Key: " + k + "value: " + v));
+
+        countries.remove("GR");
     }
 }

c16b6139c57a6b4aa32ab314256d4fe75c500b21
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-05-06 15:37:23 +0300

    feat: DateTime client created

---
+++ src/gr/aueb/cf/ch26/DateTimeClient.java
@@ -0,0 +1,40 @@
+package gr.aueb.cf.ch26;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class DateTimeClient {
+
+    public static void main(String[] args) {
+
+        Socket socket = null;
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bf = null;
+
+        try {
+            InetAddress serveAddress = InetAddress.getByName("time.nist.gov");
+            int servPort = 13;
+            socket = new Socket(serveAddress, servPort);
+            bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = "";
+
+            while ((line = bf.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            System.out.println("Server DateTime: " + sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null) socket.close();
+                if (bf != null) bf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
